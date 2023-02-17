93f8763d3a8b296dd707acfe0051e417da434028
Croissant <andrewsongda@gmail.com>
2023-02-17 13:39:33 +1100

    added remaining books

--- BookStore/Migrations/Configuration.cs
+++ BookStore/Migrations/Configuration.cs
@@ -23,6 +23,18 @@
                     Id = "9b0896fa-3880-4c2e-bfd6-925c87f22878",
                     BookName = "CQRS For Dummies",
                     Reserved = false
+                },
+                new Book
+                {
+                    Id = "0550818d-36ad-4a8d-9c3a-a715bf15de76",
+                    BookName = "Visual Studio Tips",
+                    Reserved = false
+                },
+                new Book
+                {
+                    Id = "8e0f11f1-be5c-4dbc-8012-c19ce8cbe8e1",
+                    BookName = "NHibernate Cookbook",
+                    Reserved = false
                 }
                 ); ;
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
